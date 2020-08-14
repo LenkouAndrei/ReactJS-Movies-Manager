e2478807e3e235e0a2cd6dcf52adecc72c0dc891
Andrei Lenkov <lenkow.andrei.89@gmail.com>
2020-08-14 21:38:32 +0300

    refactor/ add error boundary; strictmode

--- movies-manager/src/app/app.component.tsx
+++ movies-manager/src/app/app.component.tsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
-import { Footer, Header, Main } from "../containers";
+import { Footer, Header, Main, ErrorBoundary } from "../containers";
 import "./app.component.scss";
 
 class AppComponent extends Component {
   render() {
     return (
-      <>
-        <Header />
-        <Main />
-        <Footer />
-      </>
+      <React.StrictMode>
+        <ErrorBoundary>
+          <Header />
+          <Main />
+          <Footer />
+        </ErrorBoundary>
+      </React.StrictMode>
     );
   }
 }
--- movies-manager/src/containers/index.ts
+++ movies-manager/src/containers/index.ts
@@ -2,3 +2,4 @@ export * from './footer/footer.component';
 export * from './header/header.component';
 export * from './main/main.component';
 export * from './wrapper/wrapper.component';
+export * from './error-boundary/error-boundary.component';
